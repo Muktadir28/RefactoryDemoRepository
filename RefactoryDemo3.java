486b800fac171719c31b9a00e10936dec3b3fd91
Muktadir28 <mdmuktadir028@gmail.com>
2017-04-09 00:59:23 +0600

    Third one

---
+++ RefactoryDemo3.java
@@ -0,0 +1,37 @@
+package RefactoryDemoPacage;
+
+public class RefactoryDemo3 {
+	private MyCollection _orders = new MyCollection();
+	private String _name = "Customer Name";
+	void printOwing(){
+		Enumeration e = _orders.elements();
+		double outstanding = 0.0;
+		int count=0;
+		//print banner 
+		printBanner();
+		
+		//calculate outstanding 
+		outstanding = calculateOutstanding(e, outstanding);
+		
+		// print details
+		printDetails(outstanding);
+	}
+	private void printDetails(double outstanding) {
+		System.out.println("name: " + _name);
+		Syatem.out.println("amount: " + outstanding);
+		//System.out.println("Count: " + count)
+	}
+	private void printBanner() {
+		System.out.println("*************************");
+		System.out.println("***** Customer Owes *****");
+		System.out.println("*************************");
+	}
+	private double calculateOutstanding(Enumeration e, double outstanding) {
+		while (e.hasMoreElements()){
+			Order each = (Order) e.nextElement();
+			outstanding += each.getAmount();
+			//count++;
+		}
+		return outstanding;
+	}
+}
